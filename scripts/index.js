2ae3a92c510e7d4685392085fb9edd3118bb95cc
Damon Coffey <damoncoffey@uri.edu>
2020-12-18 17:51:58 -0500

    Javascript
    Added javascript function for our button and started creating our array of info

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,33 @@
-let animals = {
+const fish = [{
     name: "shark",
     picture: "../pictures/shark.jpg",
     info: "<h2> shark </h2> <p> This is a shark </p>"
-}
+},
+{
+    name: "mandarin-fish",
+    picture: "../pictures/mandarinfish.jpg",
+    info: "<h2> mandarin fish </h2> <p> This is a mandarin fish </p>" 
+}]
+
+function button(event)
+{ 
+    /*
+    iterating through each fish in the array and passing the animal card that is clicked on
+    */
+    var animal = event.target.id
+    var obj;
+    fish.forEach(element => {
+        if (animal == element.name){
+            obj=element
+
+        }
+        
+    });
+    var displayInfo = document.querySelector(".display-info")
+    displayInfo.innerHTML=obj.info
+    var displayPicture = document.querySelector("#display-picture")
+    displayPicture.src= obj.picture
+} 
+
+
+
